16529d10532f56b37913c90a831b9005e50ae6f7
Madeline Power <madelinepower@Madelines-MacBook-Pro.local>
2017-04-11 14:00:01 -0500

    added index.html and js

---
+++ sonnet.js
@@ -0,0 +1,28 @@
+// Take the contents of the sonnet div and place it in a variable.
+
+// Set the content of the sonnet div with the new string.
+
+var sonnet = document.getElementById('sonnet').innerHTML;
+console.log(sonnet);
+
+// Find and output the starting position of the word "orphans".
+var index = sonnet.indexOf('orphans');
+console.log("index of orphans", index);
+
+// Output the number of characters in the sonnet.
+var length = sonnet.length;
+console.log("length of sonnet", length);
+
+// Replace the first occurance of the string "winter" with "yuletide".
+
+sonnet = sonnet.replace("winter", "yuletide");
+console.log('replace winter', sonnet);
+
+// Replace all occurances of the string "the" with "a large".
+
+sonnet = sonnet.replace(/the /gi, "a large ");
+console.log("global replace", sonnet);
+
+// Set the content of the sonnet div with the new string.
+
+document.getElementById("sonnet").innerHTML = sonnet;
